59ca6ce6003e54d6108c62834e43be65bb10c73c
Nguyen Thanh Thien <131884019+thanhthienz22016@users.noreply.github.com>
2025-07-10 23:34:01 +0700

    Phát triển chỉ số phản công cho defencer

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// 
+// Tom & Jerry (counterRate: 0.8)
 
 /**
  * 
- * @param {*} name 
- * @param {*} hp 
- * @param {*} atk 
- * @param {*} defence 
- * @param {*} speed 
+ * Health / HP: Máu
+ * Attack power/ATK: Sức mạnh 
+ * Defense: Phòng thủ
+ * Speed: Tốc độ 
+ * Counter attack: Phản công 
  */
 
 
@@ -15,24 +15,42 @@
 // Không dùng Object literals nữa mà dùng "Hàm tạo" hay còn gọi là Bản thiết kế nhân vật
 
 // Tạo hàm bắt đầu trận chiến
-function Character(name, hp, atk, defence, speed) {
+function Character(name, hp, atk, defence, speed, counterRate) {
     this.name = name;
     this.hp = hp;
     this.atk = atk;
     this.defence = defence;
     this.speed = speed;
+    this.counterRate = counterRate;
+
+    // Phương thức tấn công
     this.attack = function (target) {
-        const damage = Math.max(this.atk - target.defence);
+        // Tính damage mà không âm
+        const damage = Math.max(this.atk - target.defence, 0);
+        // Trừ HP của mục tiêu
         target.hp -= damage;
         console.log(`${this.name} đang tấn công ${target.name} với chỉ số tấn công là ${damage} và ${target.name} còn ${target.hp} máu.`);
+
+        // Xử lý target phản công
+        if (target.isAlive() & Math.random() < target.counterRate) {
+                const counterDamage = Math.max(target.atk - this.defence, 0);
+
+                // Trừ "HP" của "attacker"
+                this.hp -= counterDamage;
+
+                console.log(`${target.name} phang lại ${this.name} với chỉ số tấn công là ${counterDamage} và ${this.name} còn ${this.hp} máu.`);
+
+        }
     };
     this.isAlive = function () {
         return this.hp > 0;
     };
  }
 
- const tom = new Character('Tom', 1000, 120, 30, 10);
- const jerry = new Character('Jerry', 670, 90, 21, 20);
+//  Tạo ra nhân vật Tom & Jerry
+ const tom = new Character('Tom', 1000, 120, 30, 10, 0.5);
+ const jerry = new Character('Jerry', 670, 90, 21, 20, 0.2);
+
 
 
 // ================ Object literals ======================
@@ -94,10 +112,16 @@ function battle(char1, char2) {
 
     // Tạo hàm để không bị lặp phương thức đánh nhau & linh hoạt hơn ở vòng lặp While 
     function battleRound (attacker, defencer) {
+        // Attacker đánh trước
         attacker.attack(defencer);
-        if (defencer.isAlive()) defencer.attack(attacker);
+
+        // Sau đó, nếu defender vẫn sống thì đánh lại
+        if (defencer.isAlive() && attacker.isAlive) {
+            defencer.attack(attacker);
+        } 
     }
 }
+
 
 
 //  Gọi hàm bắt đầu trận chiến
