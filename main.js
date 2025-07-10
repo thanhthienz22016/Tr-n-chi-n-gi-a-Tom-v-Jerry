47669620beb966f05d091b5d60ad36a19811f7ca
Nguyen Thanh Thien <131884019+thanhthienz22016@users.noreply.github.com>
2025-07-10 10:50:25 +0700

    Trận chiến Tom and Jerry #2 - Thay thế Object literals với hàm tạo linh hoạt hơn

--- main.js
+++ main.js
@@ -1,32 +1,55 @@
-const tom = {
-    name: 'Tom',
-    hp: 1000,
-    atk: 120,
-    defence: 30,
-    attack(target) {
-        const damage = Math.max(this.atk - target.defence);
-        target.hp -= damage;
-        console.log(`${this.name} đang tấn công ${target.name} với chỉ số tấn công là ${damage} và ${target.name} còn ${target.hp} máu.`);
-    },
-    isAlive() {
-        return this.hp > 0;
-    },
-}
-
-const jerry = {
-    name: 'Jerry',
-    hp: 670,
-    atk: 90,
-    defence: 21,
-    attack(target) {
+// Không dùng Object literals nữa mà dùng "Hàm tạo" hay còn gọi là Bản thiết kế nhân vật
+function Character(name, hp, atk, defence) {
+    this.name = name;
+    this.hp = hp;
+    this.atk = atk;
+    this.defence = defence;
+    this.attack = function (target) {
         const damage = Math.max(this.atk - target.defence);
         target.hp -= damage;
         console.log(`${this.name} đang tấn công ${target.name} với chỉ số tấn công là ${this.atk} và ${target.name} còn ${target.hp} máu.`);
-    },
-    isAlive() {
-        return this.hp > 0;
-    },
+    };
+    this.isAlive = function () {
+         return this.hp > 0;
+    };
 }
+
+// Tạo ra nhân vật Tom 
+const tom = new Character('Tom', 1000, 50, 5);
+// Tạo ra nhân vật Jerry
+const jerry = new Character('Jerry', 670, 90, 21);
+
+
+// ================ Object literals ======================
+// const tom = {
+//     name: 'Tom',
+//     hp: 1000,
+//     atk: 120,
+//     defence: 30,
+//     attack(target) {
+//         const damage = Math.max(this.atk - target.defence);
+//         target.hp -= damage;
+//         console.log(`${this.name} đang tấn công ${target.name} với chỉ số tấn công là ${damage} và ${target.name} còn ${target.hp} máu.`);
+//     },
+//     isAlive() {
+//         return this.hp > 0;
+//     },
+// }
+
+// const jerry = {
+//     name: 'Jerry',
+//     hp: 670,
+//     atk: 90,
+//     defence: 21,
+//     attack(target) {
+//         const damage = Math.max(this.atk - target.defence);
+//         target.hp -= damage;
+//         console.log(`${this.name} đang tấn công ${target.name} với chỉ số tấn công là ${this.atk} và ${target.name} còn ${target.hp} máu.`);
+//     },
+//     isAlive() {
+//         return this.hp > 0;
+//     },
+// }
 
 
 // Cho Tom và Jerry phang nhau
